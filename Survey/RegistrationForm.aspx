bd0c09890080503749797c92861372c8be89ddd7
Denis <vip.lichenko@gmail.com>
2018-03-30 22:24:48 +0300

    Основная разметка страницы

---
+++ Survey/RegistrationForm.aspx
@@ -0,0 +1,132 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="RegistrationForm.aspx.cs" Inherits="Survey.RegistrationForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+
+<!-- <link rel="stylesheet" href="~/css/style.css" /> -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"/>
+
+<title></title>
+</head>
+<body>
+    <ol class="breadcrumb">
+      <li><a href="#">Админ-панель </a></li>
+      <li class="active"> / Регистрация</li>
+    </ol>
+    <form id="form1" runat="server">
+        <div>
+            <table class="auto-style1">
+                <tr>
+                    <td class="auto-style2">Фамилия</td>
+                    <td>
+                        <asp:TextBox ID="tbLastName" runat="server" Width="200px"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style2">Имя и Отчество</td>
+                    <td>
+                        <asp:TextBox ID="tbFirstName" runat="server" Width="200px"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style2">Название конкурсной работы</td>
+                    <td>
+                        <asp:TextBox ID="tbNameCompetition" runat="server" Width="200px"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style2">Номинация конкурсной работы</td>
+                    <td>
+                        <asp:DropDownList ID="WorkNomination" runat="server" Width="200px">
+                            <asp:ListItem>Учебный проект</asp:ListItem>
+                            <asp:ListItem>Воспитательный проект</asp:ListItem>
+                            <asp:ListItem>Поза школьный проект</asp:ListItem>
+                            <asp:ListItem>Online образовательный ресурс</asp:ListItem>
+                            <asp:ListItem>Учебно-методическое</asp:ListItem>
+                        </asp:DropDownList>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style2">Дисциплина</td>
+                    <td>
+                        <asp:TextBox ID="tbDiscipline" runat="server" Width="200px"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style2">Была ли апробирована Ваша анкета (Да, Нет)?</td>
+                    <td>
+                        <asp:TextBox ID="tbTested" runat="server" Width="200px"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style2">Если да, то на каком уровне?</td>
+                    <td>
+                        <asp:TextBox ID="tbTestLVL" runat="server" Width="200px"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style2">Полное название организации в которой Вы работаете</td>
+                    <td>
+                        <asp:TextBox ID="tbCompany" runat="server" Width="200px"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style2">Ваша должность</td>
+                    <td>
+                        <asp:TextBox ID="tbPosition" runat="server" Width="200px"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style2">Город, населенный пункт</td>
+                    <td>
+                        <asp:TextBox ID="tbCity" runat="server" Width="200px"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style3">Область</td>
+                    <td class="auto-style4">
+                        <asp:TextBox ID="tbRegion" runat="server" Width="200px"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style2">Мобильный телефон</td>
+                    <td>
+                        <asp:TextBox ID="tbPhone" runat="server" Width="200px"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style2">Электронная почта</td>
+                    <td>
+                        <asp:TextBox ID="tbEmail" runat="server" Width="200px"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style2">Как Вы о нас узнали</td>
+                    <td>
+                        <asp:RadioButtonList ID="RadioButtonList1" runat="server">
+                            <asp:ListItem>Сайт МОН</asp:ListItem>
+                            <asp:ListItem>Сайт Microsoft Ukraine</asp:ListItem>
+                            <asp:ListItem>Сайт сети &quot;имя&quot;</asp:ListItem>
+                            <asp:ListItem>Еще вариант</asp:ListItem>
+                            <asp:ListItem>Еще вариант</asp:ListItem>
+                            <asp:ListItem>Еще вариант</asp:ListItem>
+                            <asp:ListItem>Еще вариант</asp:ListItem>
+                        </asp:RadioButtonList>
+                        Свой вариант:&nbsp;
+                        <asp:TextBox ID="tbOption" runat="server" Width="200px"></asp:TextBox>
+                    </td>
+                </tr>
+            </table>
+        </div>
+        <br />
+        <asp:CheckBox ID="CheckBox1" runat="server" Text="Я даю согласие на обработку моих личных данных и соглашаюсь с" />
+&nbsp;<asp:HyperLink ID="HyperLink1" runat="server">политикой конфиденциальности</asp:HyperLink>
+        <br />
+        <br />
+        <asp:Button ID="btnSend" runat="server" Text="Отправить" />
+    </form>
+</body>
+</html>
---
+++ Survey/RegistrationForm.aspx.designer.cs
@@ -0,0 +1,186 @@
+﻿//------------------------------------------------------------------------------
+// <автоматически создаваемое>
+//     Этот код создан программой.
+//
+//     Изменения в этом файле могут привести к неправильной работе и будут потеряны в случае
+//     повторной генерации кода. 
+// </автоматически создаваемое>
+//------------------------------------------------------------------------------
+
+namespace Survey {
+    
+    
+    public partial class RegistrationForm {
+        
+        /// <summary>
+        /// form1 элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// tbLastName элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox tbLastName;
+        
+        /// <summary>
+        /// tbFirstName элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox tbFirstName;
+        
+        /// <summary>
+        /// tbNameCompetition элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox tbNameCompetition;
+        
+        /// <summary>
+        /// WorkNomination элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList WorkNomination;
+        
+        /// <summary>
+        /// tbDiscipline элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox tbDiscipline;
+        
+        /// <summary>
+        /// tbTested элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox tbTested;
+        
+        /// <summary>
+        /// tbTestLVL элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox tbTestLVL;
+        
+        /// <summary>
+        /// tbCompany элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox tbCompany;
+        
+        /// <summary>
+        /// tbPosition элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox tbPosition;
+        
+        /// <summary>
+        /// tbCity элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox tbCity;
+        
+        /// <summary>
+        /// tbRegion элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox tbRegion;
+        
+        /// <summary>
+        /// tbPhone элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox tbPhone;
+        
+        /// <summary>
+        /// tbEmail элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox tbEmail;
+        
+        /// <summary>
+        /// RadioButtonList1 элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RadioButtonList RadioButtonList1;
+        
+        /// <summary>
+        /// tbOption элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox tbOption;
+        
+        /// <summary>
+        /// CheckBox1 элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CheckBox CheckBox1;
+        
+        /// <summary>
+        /// HyperLink1 элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink HyperLink1;
+        
+        /// <summary>
+        /// btnSend элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnSend;
+    }
+}
