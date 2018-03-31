db990ad0ef99d623f09e2ac953293296dc8f84f8
Denis <vip.lichenko@gmail.com>
2018-03-31 17:03:11 +0300

    Create class User

--- Survey/User.cs
+++ Survey/User.cs
@@ -7,5 +7,21 @@ namespace Survey
 {
     public class User
     {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string NameCompetition { get; set; }
+        public string WorkNominationText { get; set; }
+        public string WorkNominationValue { get; set; }
+        public string Discipline { get; set; }
+        public string Tested { get; set; }
+        public string testedLVL { get; set; }
+        public string CompanyName { get; set; }
+        public string Position { get; set; }
+        public string City { get; set; }
+        public string Region { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Statistic { get; set; }
     }
 }
