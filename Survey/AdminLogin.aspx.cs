a7b3844e32575ff7325803b195296bae9a0de9f6
Denis <vip.lichenko@gmail.com>
2018-04-10 21:33:52 +0300

    Add CustomErrors.

--- Survey/AdminLogin.aspx.cs
+++ Survey/AdminLogin.aspx.cs
@@ -11,7 +11,13 @@ namespace Survey
 {
     public partial class AdminLogin : System.Web.UI.Page
     {
-        protected void Page_Load(object sender, EventArgs e) { }
+        string clientIp = null;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            clientIp = (Request.ServerVariables["HTTP_X_FORWARDED_FOR"] ??
+                   Request.ServerVariables["REMOTE_ADDR"]).Split(',')[0].Trim();
+        }
 
         protected void Button1_Click(object sender, EventArgs e)
         {
@@ -21,14 +27,14 @@ namespace Survey
                 AdminContext db = new AdminContext();
                 var admins = db.Admins;
 
-                foreach(Admin adm in admins)
+                foreach (Admin adm in admins)
                 {
                     // Проверяем правильность введенных данных
                     if (tbLogin.Text == adm.Login && tbPassword.Text == adm.Password && tbPIN.Text == adm.Pin)
                     {
                         HttpCookie login = new HttpCookie("login", tbLogin.Text);
                         HttpCookie key = new HttpCookie("key", MD5.CreateMD5(tbPassword.Text + "l0l_ah@h@a"));
-                        
+
                         // Срок службы куки файла - 1 час
                         login.Expires.AddHours(1);
                         key.Expires.AddHours(1);
@@ -40,10 +46,12 @@ namespace Survey
                         // В случае успешной авторизации редиректим пользователя на админ-панель
                         Response.Redirect("AdminPanel.aspx");
                     }
-                    else errorLabel.Visible = true;
+                    else
+                    {
+                        errorLabel.Visible = true;
+                    }
                 }
             }
-
         }
     }
 }
